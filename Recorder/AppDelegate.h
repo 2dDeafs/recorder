a4ea8e8eeff045477b6703066d7669fbbbe98e47
Matheus Cardoso <m.d.cardoso@live.com>
2014-02-03 13:32:14 -0200

    Initial Commit

---
+++ Recorder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Recorder
+//
+//  Created by Matheus Cardoso on 2/3/14.
+//  Copyright (c) 2014 Matheus Cardoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Recorder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Recorder
+//
+//  Created by Matheus Cardoso on 2/3/14.
+//  Copyright (c) 2014 Matheus Cardoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
